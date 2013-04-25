4be0d8a19597e2d9310eae7c2156088d177aeb55
Claudia Kölbl <claudia.koelbl@hs-augsburg.de>
2013-04-25 23:24:13 +0200

    Fließender Hintergrund

--- client/prototype_claudia/js/prototype_claudia.js
+++ client/prototype_claudia/js/prototype_claudia.js
@@ -1,8 +1,6 @@
 goog.provide('prototype_claudia');
-//TEST ob Repository funktioniert
-goog.provide('lime.Label');
-goog.provide('lime.Renderer.CANVAS.LABEL');
-goog.provide('lime.Renderer.DOM.LABEL');
+
+
 goog.require('lime.Renderer.CANVAS.SPRITE');
 goog.require('lime.Renderer.DOM.SPRITE');
 goog.require('lime.Sprite');
@@ -16,7 +14,9 @@ goog.require('lime.animation.Sequence');
 goog.require('lime.animation.ScaleTo');
 goog.require('lime.animation.MoveBy');
 goog.require('lime.animation.RotateBy');
+goog.require('lime.animation.RotateTo');
 goog.require('lime.animation.Loop');
+goog.require('lime.animation.Sequence');
 goog.require('lime.RoundedRect');
 goog.require('lime.Sprite');
 
@@ -45,15 +45,20 @@ prototype_claudia.start = function(){
 	var director = new lime.Director(document.body, VIEWPORT_WIDTH, VIEWPORT_HEIGHT),
 	    
         scene = new lime.Scene(),
+
         background = new lime.Sprite().setSize(VIEWPORT_WIDTH,VIEWPORT_HEIGHT).setFill('images/wiese/Verlauf_Wiese.jpg').setPosition(0,0).setAnchorPoint(0,0),
-        erde = new lime.Sprite().setSize(1470,1470).setFill('images/wiese/Erde_Wiese.png').setPosition(0,1960).setAnchorPoint(0,1),
-        landschaft1 = new lime.Sprite().setSize(1590,1590).setFill('images/wiese/Landschaft1_Wiese.png').setPosition(0,1960).setAnchorPoint(0,1),
-        landschaft2 = new lime.Sprite().setSize(1680,1680).setFill('images/wiese/Landschaft2_Wiese.png').setPosition(0,1960).setAnchorPoint(0,1),
-        himmel = new lime.Sprite().setSize(1750,1750).setFill('images/wiese/Himmel_Wiese.png').setPosition(0,1960).setAnchorPoint(0,1),
-        planet_bottom = new lime.Circle().setSize(1475,1475).setFill('#232323').setAnchorPoint(0.5,0.5).setPosition(0,0),
-        hindernis1 = new lime.RoundedRect().setSize(100, 100).setFill(160,270,280).setAnchorPoint(0.5, 0.5).setPosition(0, -1480),
-        hindernis2 = new lime.RoundedRect().setSize(100, 160).setFill(160,270,280).setAnchorPoint(0.5, 0.5).setPosition(0, 1480),
-        hindernis3 = new lime.RoundedRect().setSize(75, 120).setFill(160,270,280).setAnchorPoint(0.5, 0.5).setPosition(1600, 0),
+        himmel1 = new lime.Sprite().setSize(1750,1750).setFill('images/wiese/Himmel_Wiese.png').setPosition(0,1960).setAnchorPoint(0,1),
+        himmel2 = new lime.Sprite().setSize(1750,1750).setFill('images/wiese/Himmel_Wiese.png').setPosition(0,1960).setAnchorPoint(0,1).setRotation(90),
+        landschaftHinten1 = new lime.Sprite().setSize(1680,1680).setFill('images/wiese/Landschaft2_Wiese.png').setPosition(0,1960).setAnchorPoint(0,1),
+        landschaftHinten2 = new lime.Sprite().setSize(1680,1680).setFill('images/wiese/Landschaft2_Wiese.png').setPosition(0,1960).setAnchorPoint(0,1).setRotation(90),
+        landschaftVorne1 = new lime.Sprite().setSize(1590,1590).setFill('images/wiese/Landschaft1_Wiese.png').setPosition(0,1960).setAnchorPoint(0,1),
+        landschaftVorne2 = new lime.Sprite().setSize(1590,1590).setFill('images/wiese/Landschaft1_Wiese.png').setPosition(0,1960).setAnchorPoint(0,1).setRotation(90),
+        erde1 = new lime.Sprite().setSize(1470,1470).setFill('images/wiese/Erde_Wiese.png').setPosition(0,1960).setAnchorPoint(0,1),
+        erde2 = new lime.Sprite().setSize(1470,1470).setFill('images/wiese/Erde_Wiese.png').setPosition(0,1960).setAnchorPoint(0,1).setRotation(90),
+
+        //hindernis1 = new lime.RoundedRect().setSize(100, 100).setFill(160,270,280).setAnchorPoint(0.5, 0.5).setPosition(0, -1480),
+        //hindernis2 = new lime.RoundedRect().setSize(100, 160).setFill(160,270,280).setAnchorPoint(0.5, 0.5).setPosition(0, 1480),
+        //hindernis3 = new lime.RoundedRect().setSize(75, 120).setFill(160,270,280).setAnchorPoint(0.5, 0.5).setPosition(1600, 0),
         character = new lime.Circle().setSize(100, 150).setPosition(100, 535).setAnchorPoint(0.5, 1).setFill('#d5622f'),
         jumpArea =  new lime.Node().setSize(VIEWPORT_WIDTH / 2, VIEWPORT_HEIGHT / 2).setPosition(0,0).setAnchorPoint(0,0),
         crouchArea = new lime.Node().setSize(VIEWPORT_WIDTH / 2, VIEWPORT_HEIGHT / 2).setPosition(0, VIEWPORT_HEIGHT / 2).setAnchorPoint(0,0),
@@ -68,46 +73,118 @@ prototype_claudia.start = function(){
         
        
 
-    //planet_bottom.appendChild(hindernis1);
-	//planet_bottom.appendChild(hindernis2);
-    //planet_bottom.appendChild(hindernis3);
-    //layer_1.appendChild(planet_bottom);
-    //layer_1.appendChild(planet_top);
-	layer_1.appendChild(background);
-	layer_1.appendChild(himmel);
-	layer_1.appendChild(landschaft2);
-	layer_1.appendChild(landschaft1);
-	layer_1.appendChild(erde);
-    layer_1.appendChild(character);
-    layer_2.appendChild(jumpArea);
-    layer_2.appendChild(crouchArea);
-            
-    //background.qualityRenderer = true;
-
-
-    scene.appendChild(layer_1);
-    scene.appendChild(layer_2);
-
-	director.makeMobileWebAppCapable();
+		layer_1.appendChild(background);
+		layer_1.appendChild(himmel2);
+		layer_1.appendChild(himmel1);
+		layer_1.appendChild(landschaftHinten2);
+		layer_1.appendChild(landschaftHinten1);
+		layer_1.appendChild(landschaftVorne2);
+		layer_1.appendChild(landschaftVorne1);
+		layer_1.appendChild(erde1);
+		layer_1.appendChild(erde2);
+	    layer_1.appendChild(character);
+	    
+	    layer_2.appendChild(jumpArea);
+	    layer_2.appendChild(crouchArea);
+	            
+	    //background.qualityRenderer = true;
+	
+	
+	    scene.appendChild(layer_1);
+	    scene.appendChild(layer_2);
+	
+		director.makeMobileWebAppCapable();
 
 
 
 ///////////////
 /* ANIMATION */
 ///////////////
+        
 
-    var turnLandschaft1 = new lime.animation.Loop(
-        new lime.animation.RotateBy(360).setDuration(60).setEasing(lime.animation.Easing.LINEAR)
+    var drehungErde1 = new lime.animation.Loop(
+    	    new lime.animation.Sequence(
+    	      new lime.animation.RotateTo(90).setDuration(20).setEasing(lime.animation.Easing.LINEAR),	
+        	  new lime.animation.RotateTo(180).setDuration(0).setEasing(lime.animation.Easing.LINEAR),
+              new lime.animation.RotateTo(270).setDuration(0).setEasing(lime.animation.Easing.LINEAR),
+              new lime.animation.RotateTo(360).setDuration(20).setEasing(lime.animation.Easing.LINEAR)              
+            )
     );
     
-    var turnLandschaft2 = new lime.animation.Loop(
-        new lime.animation.RotateBy(360).setDuration(65).setEasing(lime.animation.Easing.LINEAR)
-    );
-
-    var turnHimmel = new lime.animation.Loop(
-        new lime.animation.RotateBy(360).setDuration(100).setEasing(lime.animation.Easing.LINEAR)
+    var drehungErde2 = new lime.animation.Loop(
+    	    new lime.animation.Sequence(
+    	      new lime.animation.RotateTo(180).setDuration(0).setEasing(lime.animation.Easing.LINEAR),	
+        	  new lime.animation.RotateTo(270).setDuration(0).setEasing(lime.animation.Easing.LINEAR),
+              new lime.animation.RotateTo(360).setDuration(20).setEasing(lime.animation.Easing.LINEAR),
+              new lime.animation.RotateTo(90).setDuration(20).setEasing(lime.animation.Easing.LINEAR)              
+            )
     );
     
+    var drehungLandschaftVorne1 = new lime.animation.Loop(
+    	    new lime.animation.Sequence(
+    	      new lime.animation.RotateTo(90).setDuration(20).setEasing(lime.animation.Easing.LINEAR),	
+        	  new lime.animation.RotateTo(180).setDuration(0).setEasing(lime.animation.Easing.LINEAR),
+              new lime.animation.RotateTo(270).setDuration(0).setEasing(lime.animation.Easing.LINEAR),
+              new lime.animation.RotateTo(360).setDuration(20).setEasing(lime.animation.Easing.LINEAR)              
+            )
+    );
+    
+    var drehungLandschaftVorne2 = new lime.animation.Loop(
+    	    new lime.animation.Sequence(
+    	      new lime.animation.RotateTo(180).setDuration(0).setEasing(lime.animation.Easing.LINEAR),	
+        	  new lime.animation.RotateTo(270).setDuration(0).setEasing(lime.animation.Easing.LINEAR),
+              new lime.animation.RotateTo(360).setDuration(20).setEasing(lime.animation.Easing.LINEAR),
+              new lime.animation.RotateTo(90).setDuration(20).setEasing(lime.animation.Easing.LINEAR)              
+            )
+    );
+    
+    var drehungLandschaftHinten1 = new lime.animation.Loop(
+    	    new lime.animation.Sequence(
+    	      new lime.animation.RotateTo(90).setDuration(35).setEasing(lime.animation.Easing.LINEAR),	
+        	  new lime.animation.RotateTo(180).setDuration(0).setEasing(lime.animation.Easing.LINEAR),
+              new lime.animation.RotateTo(270).setDuration(0).setEasing(lime.animation.Easing.LINEAR),
+              new lime.animation.RotateTo(360).setDuration(35).setEasing(lime.animation.Easing.LINEAR)              
+            )
+    );
+    
+    var drehungLandschaftHinten2 = new lime.animation.Loop(
+    	    new lime.animation.Sequence(
+    	      new lime.animation.RotateTo(180).setDuration(0).setEasing(lime.animation.Easing.LINEAR),	
+        	  new lime.animation.RotateTo(270).setDuration(0).setEasing(lime.animation.Easing.LINEAR),
+              new lime.animation.RotateTo(360).setDuration(35).setEasing(lime.animation.Easing.LINEAR),
+              new lime.animation.RotateTo(90).setDuration(35).setEasing(lime.animation.Easing.LINEAR)              
+            )
+    );
+    
+    var drehungHimmel1 = new lime.animation.Loop(
+    	    new lime.animation.Sequence(
+    	      new lime.animation.RotateTo(90).setDuration(50).setEasing(lime.animation.Easing.LINEAR),	
+        	  new lime.animation.RotateTo(180).setDuration(0).setEasing(lime.animation.Easing.LINEAR),
+              new lime.animation.RotateTo(270).setDuration(0).setEasing(lime.animation.Easing.LINEAR),
+              new lime.animation.RotateTo(360).setDuration(50).setEasing(lime.animation.Easing.LINEAR)              
+            )
+    );
+    
+    var drehungHimmel2 = new lime.animation.Loop(
+    	    new lime.animation.Sequence(
+    	      new lime.animation.RotateTo(180).setDuration(0).setEasing(lime.animation.Easing.LINEAR),	
+        	  new lime.animation.RotateTo(270).setDuration(0).setEasing(lime.animation.Easing.LINEAR),
+              new lime.animation.RotateTo(360).setDuration(50).setEasing(lime.animation.Easing.LINEAR),
+              new lime.animation.RotateTo(90).setDuration(50).setEasing(lime.animation.Easing.LINEAR)              
+            )
+    );    
+    erde1.runAction(drehungErde1);
+    erde2.runAction(drehungErde2);
+    landschaftHinten1.runAction(drehungLandschaftHinten1);
+    landschaftHinten2.runAction(drehungLandschaftHinten2);
+    landschaftVorne1.runAction(drehungLandschaftVorne1);
+    landschaftVorne2.runAction(drehungLandschaftVorne2);
+    himmel1.runAction(drehungHimmel1);
+    himmel2.runAction(drehungHimmel2);
+       
+
+
+
    
     var jumpUp = new lime.animation.MoveBy(0, -280).setDuration(0.2).setEasing(lime.animation.Easing.EASEOUT);
     var jumpDown = jumpUp.reverse().setDuration(0.35).setEasing(lime.animation.Easing.EASEIN);
@@ -116,10 +193,7 @@ prototype_claudia.start = function(){
     var crouch = new lime.animation.ScaleTo(1.6, 0.5).setDuration(0.1);
     var standUp = new lime.animation.ScaleTo(1, 1).setDuration(0.1);
 
-    landschaft1.runAction(turnLandschaft1);
-    landschaft2.runAction(turnLandschaft2);
-    erde.runAction(turnLandschaft1);
-    himmel.runAction(turnHimmel);
+
 
 
     
@@ -140,41 +214,6 @@ prototype_claudia.start = function(){
 			character.runAction(standUp);
         });
     });
-   
-	/*
-    var touchStartY;
-    var touchMoveY;
-    var touchEndY;
-    var isCrouching = false;
-
-    goog.events.listen(swipeArea, 'touchstart', function(e) {
-        touchStartY = Math.round(e.position.y);
-    });
-
-    goog.events.listen(swipeArea, 'touchend', function(e) {
-        touchEndY = Math.round(e.position.y);
-        
-        if (touchEndY < touchStartY) {
-            character.runAction(jump);
-        } 
-        if (isCrouching == true) {
-            character.runAction(standUp);
-            isCrouching = false;
-        } else {
-            return;
-        }
-    });
-
-    goog.events.listen(swipeArea, 'touchmove', function(e) {
-        touchMoveY = Math.round(e.position.y);
-
-        if (isCrouching == false && touchStartY < touchMoveY) {
-            character.runAction(crouch);
-            isCrouching = true;
-        } else {
-            return;
-        }
-    }); */
 
 	director.replaceScene(scene);
 }
