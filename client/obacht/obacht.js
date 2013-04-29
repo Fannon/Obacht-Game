86a7b837da6b1190bc4ee805a056bb1723fd7441
Fannon <heimlersimon@gail.com>
2013-04-29 12:43:23 +0200

    Reintegrated Structure Example

--- client/obacht/obacht.js
+++ client/obacht/obacht.js
@@ -13,9 +13,55 @@ goog.require('lime.animation.FadeTo');
 goog.require('lime.animation.ScaleTo');
 goog.require('lime.animation.MoveTo');
 
+// obacht requirements
+goog.require('obacht.MultiplayerService');
+goog.require('obacht.Game');
+goog.require('obacht.options');
+
 
 // entrypoint
 obacht.start = function(){
+
+    "use strict";
+    console.log('Obacht start()');
+
+    console.log('Current Options:');
+    console.dir(obacht.options);
+
+
+    var viewportWidth = obacht.options.graphics.viewportWidth;
+    var viewportHeight = obacht.options.graphics.viewportHeight;
+
+    var obachtDirector = new lime.Director(document.body, viewportWidth, viewportHeight);
+    var scene = new lime.Scene(),
+        layer = new lime.Layer();
+
+    /*hier muss dann das Menu eingeschoben werden dass dann das Spiel neue Spiel aufruft
+     this.menu = new obacht.Menu(); */
+
+    this.currentGame = new obacht.Game();
+
+    //nur zu Testzwecken, wird dann ins Menu ausgelagert, sonst muss man immer durchs Menü navigieren wenn man zum Spiel will
+    layer.appendChild(this.currentGame);
+    scene.appendChild(layer);
+
+    obachtDirector.makeMobileWebAppCapable();
+    // set current scene active
+    obachtDirector.replaceScene(scene);
+
+    // Connect to Multiplayer Server
+    this.multiplayerService = new obacht.MultiplayerService(obacht.options.server.url);
+
+
+
+
+
+
+
+
+
+
+    // Altes Hello World Exampe
 
 	var director = new lime.Director(document.body,1024,768),
 	    scene = new lime.Scene(),
