43aa5a98575f42bf4de4c3f1f7209309f6ed68cd
Sebastian Huber <blacklist.mod@web.de>
2013-04-30 16:07:21 +0200

    updated the client multiplayerservice - tested - worked

--- client/obacht/MultiplayerService.js
+++ client/obacht/MultiplayerService.js
@@ -8,11 +8,9 @@ goog.provide('obacht.MultiplayerService');
  * It includes the communication protocol and registers functions and events
  * Uses Socket.io and connects to a Node.js Server
  *
- * @singleton
  * @author Simon Heimler
+ * @author Sebastian Huber
  */
-
-// TODO: Singleton Pattern: http://addyosmani.com/resources/essentialjsdesignpatterns/book/#singletonpatternjavascript
 
 obacht.MultiplayerService = function(serverUrl) {
 
@@ -22,6 +20,7 @@ obacht.MultiplayerService = function(serverUrl) {
 
     this.serverUrl = serverUrl;
     this.room = false;
+    this.pid = undefined;
     this.socket = io.connect(this.serverUrl); // Set up Socket-Connection to Server
 
     var self = this;
@@ -33,25 +32,45 @@ obacht.MultiplayerService = function(serverUrl) {
     // Communication Events     //
     //////////////////////////////
 
-    this.socket.on('connected', function (success) {
-        if (success === true) {
+    this.socket.on('connected', function (data) {
+        if (!data.error) {
             console.log('Successful Connected');
-        }
-    });
-
-    this.socket.on('room_pin', function (pin) {
-        if (pin) {
-            self.room = pin;
-            console.log('Joined Room #' + pin);
+            self.pid = data.pid;
         } else {
-            console.log('Could not join room, maybe it is full.');
+            console.log(data.error);
         }
     });
 
-    this.socket.on('new_room_pin', function (pin) {
-        console.log('New PIN received: ' + pin);
-        this.joinRoom(pin);
+    this.socket.on('room_detail', function (data) {
+        console.dir(data);
+        if (data.error) {
+            // e.g. no free place in room
+            console.log(data.error);
+        } else {
+            // if no errors
+            if (data.players.length === 0 && data.pin === 0) {
+                // if random room has no players yet
+                console.log('Created new Room');
+                console.log(data.closed);
+                self.newRoom(data.theme, data.options, data.closed);
+            } else if (data.players.length === 0) {
+                // if normal room has no players yet
+                console.log('Joined Room #' + data.pin);
+                self.joinRoom(data.pin, data.closed);
+            } else if (data.players[0] === self.pid) {
+                // if room has only first player within
+                console.log('Wait for other Player.');
+            } else if (data.players.length === 1) {
+                // if room has room for one more player
+                console.log('Joining Room.');
+                self.joinRoom(data.pin);
+            }  
+            self.room = data.pin;   
+        }
     });
+
+
+
 
     this.socket.on('no_match_found', function () {
         console.log('No Match found.');
@@ -88,23 +107,37 @@ obacht.MultiplayerService = function(serverUrl) {
 // Communication Functions  //
 //////////////////////////////
 
+obacht.MultiplayerService.prototype.newRoom = function (theme, options, closed) {
+    console.log('>> newRoom()');
+    this.socket.emit('new_room', {
+        theme: theme,
+        options: options,
+        closed: closed
+    });
+};
+
+obacht.MultiplayerService.prototype.joinRoom = function(pin, closed) {
+    console.log('>> joinRoom(' + pin + ')');
+    this.socket.emit('join_room', {
+        pin: pin,
+        closed: closed
+    });
+};
+
 obacht.MultiplayerService.prototype.findMatch = function () {
     console.log('>> findMatch()');
     this.socket.emit('find_match');
 };
 
-obacht.MultiplayerService.prototype.newRoom = function () {
-    console.log('>> newRoom()');
-    this.socket.emit('new_room');
+obacht.MultiplayerService.prototype.playerAction = function(type, data) {
+    this.socket.emit('player_action', {
+        type: type,
+        data: data
+    });
 };
 
-obacht.MultiplayerService.prototype.joinRoom = function(pin, privateRoom) {
-    if (privateRoom) {
-        pin = 'P' + pin;
-    }
-    console.log('>> joinRoom(' + pin + ')');
-    this.socket.emit('join_room', pin);
-};
+
+
 
 obacht.MultiplayerService.prototype.leaveRoom = function() {
     console.log('>> leaveRoom()');
