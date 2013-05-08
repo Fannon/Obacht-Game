81c69ea2069c8fbb18303cdb4cef867a2b1e0fbb
FabianIAM12 <fabian.b89@gmail.com>
2013-05-08 11:05:01 +0200

    Objectübergabe an Algorithmus funktioniert

--- client/prototype_fabi/prototype_fabi.js
+++ client/prototype_fabi/prototype_fabi.js
@@ -17,7 +17,6 @@ goog.require('lime.animation.Loop');
 var VIEWPORT_WIDTH = 1280;
 var VIEWPORT_HEIGHT = 720
 
-
 // entrypoint
 prototype_fabi.start = function(){
 
@@ -40,14 +39,9 @@ prototype_fabi.start = function(){
     button2 = new lime.Sprite().setSize(150,150).setFill('#c00').setPosition(bx-180,by).setAnchorPoint(0,0);
     button3 = new lime.Sprite().setSize(150,150).setFill('#c00').setPosition(bx-360,by).setAnchorPoint(0,0);
 
-	//Labels
-	var lbl = new lime.Label().setText('Your Score: 10').setFontFamily('Verdana').
-    setFontColor('#c00').setFontSize(18).setFontWeight('bold').setPosition(200,120)
-    
-	var lbl2 = new lime.Label().setText('Your Score: 10').setFontFamily('Verdana').
+    var lbl2 = new lime.Label().setText('Your Score: 10').setFontFamily('Verdana').
     setFontColor('#c00').setFontSize(18).setFontWeight('bold').setPosition(200,60)
-    
-    
+
     // LAYER
     layer_1 = new lime.Layer().setSize(VIEWPORT_WIDTH, VIEWPORT_HEIGHT),
     layer_2 = new lime.Layer().setSize(VIEWPORT_WIDTH, VIEWPORT_HEIGHT);
@@ -57,7 +51,6 @@ prototype_fabi.start = function(){
     layer_1.appendChild(character);
     layer_1.appendChild(hindernis);
     
-    layer_1.appendChild(lbl);
     layer_1.appendChild(lbl2);
     
     layer_2.appendChild(jumpArea);
@@ -72,19 +65,39 @@ prototype_fabi.start = function(){
 
     director.makeMobileWebAppCapable();
 
-    //Kollisionsalgorithmus
-    //Parameter
-    //
-    // linke obere ecke koordinaten als array ([0]=x [1]=y),länge,hohe
-    //
-    function kollisioncheck(position1,width1,height1,position2,width2,height2){   
+    //Übergabeparameter 2 Objekte
+    function kollisionscheck(object1,object2){
+
+    //Position der 2 Objekte bestimmen
+    object1pos=object1.getPosition();
+    object2pos=object2.getPosition();
     	    
-     return 	position1[0] < position2[0] + width2  &&
-     		position2[0] < position1[0] + width1  &&
-     		position1[1] < position2[1] + height2 &&
-     		position2[1] < position1[1] + height1;  	    
-     
-    }       
+    //Aray mit X und Y Koordinaten anlegen
+    var obj1 = new Array();
+    var obj2 = new Array();
+    
+    ////Objekt1 linke obere Ecke
+    obj1[0]=Math.round(object1pos.x/10)*10-(object1.getSize().width/2);
+    obj1[1]=Math.round(object1pos.y/10)*10-(object1.getSize().height/2);
+    
+    //Weite und Höhe
+    obj1w=hindernis.getSize().width
+    obj1h=hindernis.getSize().height
+    
+    ////Objekt2 linke obere Ecke 
+    obj2[0]=Math.round(object2pos.x/10)*10-(object2.getSize().width/2);
+    obj2[1]=Math.round(object2pos.y/10)*10-(object2.getSize().height/2);
+    
+    //Weite und Höhe
+    obj2w=object2.getSize().width;
+    obj2h=object2.getSize().height;	    
+    	    
+    return 	obj1[0] < obj2[0] + obj2w  &&
+		obj2[0] < obj1[0] + obj1w  &&
+		obj1[1] < obj2[1] + obj2h &&
+		obj2[1] < obj1[1] + obj1h;  	 	    
+    }
+    
     
 //Beschleunigungsparameter
 var velocity = 0.3;
@@ -114,33 +127,9 @@ lime.scheduleManager.schedule(function(dt){
     
     /*Positionen abrufen*/
     hindernispos=hindernis.getPosition();
-    characterpos=character.getPosition();
-
-    /*ACHTUNG: Da viele Zwischenwerte fehlen sollte man auf 10er Stellen aufrunden*/
-    rhpos=Math.round(hindernispos.x/10)*10
     
-    /*Koordinaten der Ecken ARRAY[0]=X ARRAY[1]=Y*/    
-    var hind = new Array();
-    var chara = new Array();
-    
-    ////Hindernis
-    hind[0]=Math.round(hindernispos.x/10)*10-(hindernis.getSize().width/2);
-    hind[1]=Math.round(hindernispos.y/10)*10-(hindernis.getSize().height/2);
-    
-    //Weite und Höhe
-    hindw=hindernis.getSize().width
-    hindh=hindernis.getSize().height
-    
-    ////Character
-    chara[0]=Math.round(characterpos.x/10)*10-(character.getSize().width/2);
-    chara[1]=Math.round(characterpos.y/10)*10-(character.getSize().height/2);
-    
-    //Weite und Höhe
-    charaw=character.getSize().width;
-    charah=character.getSize().height;
-
     //Kollisionserkennung
-    if (kollisioncheck(hind,hindw,hindh,chara,charaw,charah)==true){
+    if (kollisionscheck(hindernis,character)==true){
     winkel=startwinkel;
     kollanz++;
     
@@ -159,10 +148,8 @@ lime.scheduleManager.schedule(function(dt){
     //Winkel erhöhen
     winkel=winkel+winkelgeschwindigkeit;
 	
-    //Text Kollisionsanzahl
-    lbl2.setText('Kollision nr: '+kollanz);
-    //Characterposition Anzeigen
-    lbl.setText(hind[0]+' / '+hind[1]);
+    //Texte Anzeigen
+    lbl2.setText('Kollision: '+kollanz);
     
 },hindernis);
 
@@ -190,8 +177,6 @@ lime.scheduleManager.schedule(function(dt){
 /* EVENTHANDLING */
 ////////////////////
 
-	
-	
     var isCrouching = false;
 
     goog.events.listen(jumpArea,['mousedown','touchstart'], function(e) {
