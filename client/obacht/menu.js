2132be6b0dcddd67771b5455d1933d540709e4e4
Fannon <heimlersimon@gmail.com>
2013-05-13 14:20:48 +0200

    Added Submit Button to JoinGame Scene, Game Menu Refactoring, Documentation++

--- client/obacht/menu.js
+++ client/obacht/menu.js
@@ -16,11 +16,10 @@ goog.provide('obacht.Menu');
 obacht.Menu = function() {
     "use strict";
 
+    // Start first Menu Scene
     this.mainMenuScene();
 
 };
-
-
 
 obacht.Menu.prototype = {
 
@@ -53,7 +52,6 @@ obacht.Menu.prototype = {
      */
     newGameScene: function() {
         "use strict";
-
         var that = this;
 
         var sceneMenu = new lime.Scene();
@@ -68,14 +66,14 @@ obacht.Menu.prototype = {
         var btn_playOnline = new obacht.Button('PLAY ONLINE').setPosition(450, 360).setSize(300, 100);
         layerMenu.appendChild(btn_playOnline);
         goog.events.listen(btn_playOnline, lime.Button.Event.CLICK, function() {
-            that.loadGame();
+            that.loadGame(false);
         });
 
         //Create-Button
         var btn_createGame = new obacht.Button('CREATE GAME').setPosition(830, 290).setSize(300, 100);
         layerMenu.appendChild(btn_createGame);
         goog.events.listen(btn_createGame, lime.Button.Event.CLICK, function() {
-            that.loadGame();
+            that.loadGame(false);
         });
 
         //Join-Button
@@ -85,12 +83,17 @@ obacht.Menu.prototype = {
             that.join();
         });
     },
+
     /**
      * Game Loading Scene
      * Starts the Game and everything that needs to be ready for that.
      */
-    loadGame: function() {
+    loadGame: function(pin) {
         "use strict";
+
+        if (!pin) {
+            pin = '1234'; // TODO: PIN from Server
+        }
 
         // Connect to Multiplayer Server
         //obacht.mp = new obacht.MultiplayerService(obacht.options.server.url);
@@ -99,68 +102,115 @@ obacht.Menu.prototype = {
         // set current scene active
         obacht.director.replaceScene(sceneGame);
 
+
+        /////////////////////////////
+        // Start new Game          //
+        /////////////////////////////
+
         obacht.playerController = new obacht.PlayerController();
 
         obacht.currentGame = new obacht.Game();
+        obacht.currentGame.pin = pin;
         sceneGame.appendChild(obacht.currentGame.layer);
 
     },
+
     /**
      * Join Custom Game Scene
      */
     join: function() {
         "use strict";
+        var that = this;
 
-        //Create Keys
-        var makeNumberKey = function(x, number) {
+
+        /////////////////////////////
+        // Variables               //
+        /////////////////////////////
+
+        /** Array for PIN */
+        var codeArray = ['_', '_', '_', '_'];
+        var codeposition = 0;
+
+        // KeyboardButton Placement
+        var startFrom = 200;
+        var spacing = 90;
+        var keys = [];
+
+
+        /////////////////////////////
+        // Helper Functions        //
+        /////////////////////////////
+
+        /**
+         * Returns PIN
+         *
+         * @returns {*}
+         */
+        var getPin = function() {
+            return codeArray[0] + codeArray[1] + codeArray[2] + codeArray[3];
+        };
+
+        /**
+         * Returns PIN with adequate spacing
+         * @returns {string}
+         */
+        var getStyledPin = function() {
+            return codeArray[0] + ' ' + codeArray[1] + ' ' + codeArray[2] + ' ' + codeArray[3];
+        };
+
+        /**
+         * Draws Button for Keyboard Input
+         *
+         * @param {integer} x
+         * @param {string} text
+         *
+         * @returns {*}
+         */
+        var drawKeyboardButton = function(x, text) {
             var key = new lime.RoundedRect().setFill('#888').setRadius(15).setPosition(x, 550).setSize(70, 70);
-            key.label = new lime.Label().setAlign('center').setText(number).setFontColor('#fff').setFontSize(40).setSize(70, 45).setFontWeight('bold');
+            key.label = new lime.Label().setAlign('center').setText(text).setFontColor('#fff').setFontSize(40).setSize(70, 45).setFontWeight('bold');
             key.appendChild(key.label);
             return key;
         };
 
-        //Add Number
+        /**
+         * Add Number to PIN
+         *
+         * @param {integer} insertNumber
+         */
         var addNumber = function(insertNumber) {
             codeArray[codeposition] = insertNumber;
             if (codeposition < 4) {
                 codeposition++;
             }
-            code = codeArray[0] + codeArray[1] + codeArray[2] + codeArray[3];
-            codeVisualization = codeArray[0] + ' ' + codeArray[1] + ' ' + codeArray[2] + ' ' + codeArray[3];
-            field.label.setText(codeVisualization);
+            field.label.setText(getStyledPin());
         };
 
-        //Delete Number
+        /**
+         * Backspace PIN (Deletes last entered PIN)
+         */
         var deleteNumber = function() {
             codeArray[codeposition - 1] = '_';
-
             if (codeposition > 0) {
                 codeposition--;
             }
-            code = codeArray[0] + codeArray[1] + codeArray[2] + codeArray[3];
-            codeVisualization = codeArray[0] + ' ' + codeArray[1] + ' ' + codeArray[2] + ' ' + codeArray[3];
-            field.label.setText(codeVisualization);
+            field.label.setText(getStyledPin());
+        };
+
+        var startGame = function() {
+            if (codeposition === 4) {
+                that.loadGame(getPin());
+            }
         };
 
 
-        var codeArray = [4];
-        codeArray[0] = '_';
-        codeArray[1] = '_';
-        codeArray[2] = '_';
-        codeArray[3] = '_';
+        /////////////////////////////
+        // Scene Content           //
+        /////////////////////////////
 
-        //Position of the Code to change
-        var codeposition = 0;
-
-        //Complete Code to Check
-        var code = codeArray[0] + codeArray[1] + codeArray[2] + codeArray[3];
-
-        //Code with Spaces for visualization
-        var codeVisualization = codeArray[0] + ' ' + codeArray[1] + ' ' + codeArray[2] + ' ' + codeArray[3];
-
-        //Code-Field
+        // Current Code Field
         var field = new lime.RoundedRect().setFill('#888').setRadius(15).setPosition(640, 360).setSize(300, 130);
-        field.label = new lime.Label().setAlign('center').setText(codeVisualization).setFontColor('#eef').setFontSize(80).setSize(300, 80).setFontWeight('bold');
+        field.label = new lime.Label().setAlign('center').setText(getStyledPin()).setFontColor('#eef').setFontSize(80).setSize(300, 80).setFontWeight('bold');
 
         field.appendChild(field.label);
         var sceneMenu = new lime.Scene();
@@ -173,29 +223,27 @@ obacht.Menu.prototype = {
 
         layerMenu.appendChild(field);
 
-        var startFrom = 125;
-        var spacing = 80;
-        var keys= [];
-
 
         /////////////////////////////
         // Create Buttons          //
         /////////////////////////////
 
         for (var i = 0; i < 10; i++) {
-            keys[i] = makeNumberKey(startFrom + spacing * i, i);
+            keys[i] = drawKeyboardButton(startFrom + spacing * i, i);
             layerMenu.appendChild(keys[i]);
         }
 
-        var keyDelete = makeNumberKey(925, '<');
+        var keyDelete = drawKeyboardButton(startFrom + spacing * 10, '<');
         layerMenu.appendChild(keyDelete);
+
+        var submitButton = drawKeyboardButton(startFrom + spacing * 11, '#');
+        layerMenu.appendChild(submitButton);
 
 
         /////////////////////////////
         // Register EventListeners //
         /////////////////////////////
 
-        // Listener for number-keys
         goog.events.listen(keys[0], lime.Button.Event.CLICK, function() {
             addNumber('0');
         });
@@ -228,6 +276,9 @@ obacht.Menu.prototype = {
         });
         goog.events.listen(keyDelete, lime.Button.Event.CLICK, function() {
             deleteNumber();
+        });
+        goog.events.listen(submitButton, lime.Button.Event.CLICK, function() {
+            startGame();
         });
 
     }
