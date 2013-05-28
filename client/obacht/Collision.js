bceba545ad2d53c035ef485e881a49f900179f53
FabianIAM12 <fabian.b89@gmail.com>
2013-05-28 10:58:25 +0200

    Refactor obacht.Collision Class

--- client/obacht/Collision.js
+++ client/obacht/Collision.js
@@ -6,35 +6,37 @@ goog.provide('obacht.Collision');
 /**
  * Collision Detector
  *
- * @param object1
- * @param object2
  * @constructor
  */
-obacht.Collision = function(object1,object2){
-
-    this.object1pos = object1.getPosition();
-    this.object2pos = object2.getPosition();
-
-    this.obj1x = Math.round(this.object1pos.x/10) * 10 - (object1.getSize().width/2);
-    this.obj1y = Math.round(this.object1pos.y/10) * 10 - (object1.getSize().height/2);
-
-    this.obj1w = object1.getSize().width;
-    this.obj1h = object1.getSize().height;
-
-    this.obj2x = Math.round(this.object2pos.x/10) * 10 - (object2.getSize().width/2);
-    this.obj2y = Math.round(this.object2pos.y/10) * 10 - (object2.getSize().height/2);
-
-    this.obj2w = object2.getSize().width;
-    this.obj2h = object2.getSize().height;
-
+obacht.Collision = function () {
+    "use strict";
 };
 
 obacht.Collision.prototype = {
-    rect: function() {
+    rect: function (object1, object2) {
         "use strict";
+        /*Object Position*/
+        this.object1pos = object1.character.getPosition();
+        this.object2pos = object2.character.getPosition();
+
+        /*Size Object1*/
+        this.obj1w = object1.character.getSize().width;
+        this.obj1h = object1.character.getSize().height;
+
+        /*Size Object2*/
+        this.obj2w = object2.character.getSize().width;
+        this.obj2h = object2.character.getSize().height;
+
+        /*Left Top Corner*/
+        this.obj1x = Math.round(this.object1pos.x / 10) * 10 - (this.obj1w / 2);
+        this.obj1y = Math.round(this.object1pos.y / 10) * 10 - (this.obj1h / 2);
+
+        /*Left Top Corner*/
+        this.obj2x = Math.round(this.object2pos.x / 10) * 10 - (this.obj2w / 2);
+        this.obj2y = Math.round(this.object2pos.y / 10) * 10 - (this.obj2h / 2);
         return this.obj1x < this.obj2x + this.obj2w  &&
-		this.obj2x < this.obj1x + this.obj1w  &&
-		this.obj1y < this.obj2y + this.obj2h &&
-		this.obj2y < this.obj1y + this.obj1h;
+            this.obj2x < this.obj1x + this.obj1w  &&
+            this.obj1y < this.obj2y + this.obj2h &&
+            this.obj2y < this.obj1y + this.obj1h;
     }
 };
