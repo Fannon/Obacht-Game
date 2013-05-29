36016cc26f18fb6cf94a1688e07a6e64c1badb25
FabianIAM12 <fabian.b89@googlemail.com>
2013-05-29 16:11:08 +0200

    Changed traps.js like bonus.js

--- client/obacht/Trap.js
+++ client/obacht/Trap.js
@@ -11,7 +11,6 @@ goog.require('obacht.options');
 
 goog.require('lime.Sprite');
 
-
 /**
  * Trap Object. Traps can be thrown by a player or ocur computer-generated.
  *
@@ -21,23 +20,28 @@ obacht.Trap = function(theme, type) {
 
     console.log('New Trap();');
 
-    var startposx = obacht.options.trap.own.x;
-    var startposy = obacht.options.trap.own.y;
-    var anchorx = obacht.options.trap.own.anchorx;
-    var anchory = obacht.options.trap.own.anchory;
+    ////////////////
+    // ATTRIBUTES //
+    ////////////////
+    var self = this;
+    this.type = type;
+    this.theme = theme;
+    this.fill = obacht.themes[this.theme].traps[this.type]+'.png';
 
-    console.log(theme + ' .. ' + type);
-
-    var file = obacht.themes[theme].traps[type].file;
-    var width = obacht.themes[theme].traps[type].width;
-    var height = obacht.themes[theme].traps[type].height;
-    var boundingBoxes = obacht.themes[theme].traps[type].boundingBoxes;
-
-    this.character = new lime.Sprite().setSize(width, height).setPosition(startposx, startposy).setAnchorPoint(anchorx, anchory).setFill(file);
+    ////////////////////
+    // LIMEJS OBJECTS //
+    ////////////////////
+    this.trap = new lime.RoundedRect().setSize(obacht.options.bonus.general.size, obacht.options.bonus.general.size).setPosition(obacht.options.bonus.general.x, obacht.options.bonus.general.y).setFill(this.fill).setAnchorPoint(0, 0).setRadius(15);
     this.layer = new lime.Layer().setSize(obacht.options.graphics.VIEWPORT_WIDTH, obacht.options.graphics.VIEWPORT_HEIGHT);
-    this.layer.appendChild(this.character);
+    self.drawBonus();
 };
 
 obacht.Trap.prototype = {
-
+    /**
+     * Draw the bonus-button
+     */
+    drawBonus: function() {
+        "use strict";
+        this.layer.appendChild(this.trap);
+    }
 };
