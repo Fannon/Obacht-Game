059949de1c38dfb67a9b33981d5ee322f79cb73f
Martin Hofmann <martin_hofmann@pool8021x168-141.rz.hs-augsburg.de>
2013-05-29 17:38:06 +0200

    stuff in white lines demo

--- client/white_lines_demo/white_lines_demo.js
+++ client/white_lines_demo/white_lines_demo.js
@@ -20,16 +20,14 @@ white_lines_demo.start = function() {
 
     var director = new lime.Director(document.body, VIEWPORT_WIDTH, VIEWPORT_HEIGHT);
     var scene = new lime.Scene().setSize(VIEWPORT_WIDTH, VIEWPORT_HEIGHT);
-    var circle1 = new lime.Sprite().setSize(800, 800).setFill('landscapeB_new.png').setPosition((VIEWPORT_WIDTH / 2), (VIEWPORT_HEIGHT / 2)).setAnchorPoint(0.5, 0.5).setRenderer(lime.Renderer.CANVAS);
-    var circle2 = new lime.Sprite().setSize(800, 800).setFill('landscapeB_new.png').setPosition((VIEWPORT_WIDTH / 2) + 200, (VIEWPORT_HEIGHT / 2) + 200).setAnchorPoint(0.5, 0.5).setRenderer(lime.Renderer.CANVAS);
-
+    var circle1 = new lime.Sprite().setSize(800, 800).setFill('landscapeB.png').setPosition((VIEWPORT_WIDTH / 2), (VIEWPORT_HEIGHT / 2)).setAnchorPoint(0.5, 0.5).setRenderer(lime.Renderer.CANVAS);
+    var circle2 = new lime.Sprite().setSize(800, 800).setFill('landscapeB.png').setPosition((VIEWPORT_WIDTH / 2) + 200, (VIEWPORT_HEIGHT / 2) + 200).setAnchorPoint(0.5, 0.5).setRenderer(lime.Renderer.CANVAS);
     var layer = new lime.Layer().setSize(VIEWPORT_WIDTH, VIEWPORT_HEIGHT);
 
     scene.appendChild(circle1);
     scene.appendChild(circle2);
 
     var turn = new lime.animation.RotateBy(360).setDuration(10).setEasing(lime.animation.Easing.LINEAR).enableOptimizations();
-
 
     circle2.runAction(turn);
 
